1005787f769b0b6a467b795a2e9451e3a34af291
Rohan Bhushan <rbhushans@gmail.com>
2021-05-19 18:28:42 -0700

    small changes

--- app/screens.py
+++ app/screens.py
@@ -72,7 +72,6 @@ def test_model():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
 # ! Tests for DB
